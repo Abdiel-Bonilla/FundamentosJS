4d1cef2619d0e460e30dcaf3c2c4661c88784e70
Abdiel-Bonilla <abboro123@gmail.com>
2024-09-12 13:31:47 -0600

    01 Tipos de Dato Java

---
+++ app.js
@@ -0,0 +1,2 @@
+console.log("Hello word!!!")
+alert("Hello word!!!")
---
+++ concepts/datatypes.js
@@ -0,0 +1,13 @@
+// TIPOS DE DATOS
+
+//NUMERICOS ENTEROS DECIMALES
+console.log(23);
+console.log(typeof 23);
+console.log(3.1416);
+console.log(typeof 3.1416);
+
+//BOOLEANOS (TRUE,FALSE)
+console.log(true);
+console.log(typeof true);
+console.log(false);
+console.log(typeof false);
