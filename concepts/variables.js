d076da1320226d7244b92f2436f1bfcca1af82f8
Abdiel-Bonilla <abboro123@gmail.com>
2024-09-12 13:57:51 -0600

    03 Variables let y const

---
+++ concepts/variables.js
@@ -0,0 +1,18 @@
+//DECLARACION DE VARIABLES CON LET Y CONST
+// var en desuso
+
+//variables let; cambiaran de valor
+let age; //declaracion
+console.log(age);
+age = 23;//inicializacion
+console.log(age);
+
+let name = "Abdiel"; //declaracion e inicializacion
+console.log(name);
+name="Isai";
+console.log(name);
+
+// constantes const:no cambiaran de valor
+const PI = 3.1416;
+//PI = 3.15;
+console.log(PI);
